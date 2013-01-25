24ef069aebccd034bb5e997908c9791dde046dc6
yangjunde <363167160@qq.com>
2013-01-25 19:19:28 +0800

    IndexController

---
+++ web/src/main/java/com/jared/web/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.jared.web.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junde.yang
+ * Date: 13-1-25
+ * Time: 下午7:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class IndexController {
+}
