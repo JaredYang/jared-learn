18cd4b8f152e2f5d7524ea711d3049b79adcdcce
jaredyang <363167160@qq.com>
2013-01-26 18:53:38 +0800

    Bean

--- web/src/main/java/com/jared/web/controller/IndexController.java
+++ web/src/main/java/com/jared/web/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.jared.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created with IntelliJ IDEA.
  * User: junde.yang
@@ -7,5 +10,8 @@ package com.jared.web.controller;
  * Time: 下午7:18
  * To change this template use File | Settings | File Templates.
  */
+@Controller
+@RequestMapping("jared")
 public class IndexController {
+
 }
