f8b9b4e6cf8b386d77d6bb97bac95d3bafad0ba4
jaredyang <363167160@qq.com>
2013-01-26 13:30:00 +0800

    Bean

---
+++ web/src/main/java/com/jared/web/bean/UserBean.java
@@ -0,0 +1,11 @@
+package com.jared.web.bean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junde.yang
+ * Date: 13-1-26
+ * Time: 下午1:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserBean {
+}
