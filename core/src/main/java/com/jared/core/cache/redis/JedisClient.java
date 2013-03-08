39d157a6d083c85d8853a0bd088efe0382b493f5
yangjunde <363167160@qq.com>
2013-03-08 17:57:25 +0800

    config file

--- core/src/main/java/com/jared/core/cache/redis/JedisClient.java
+++ core/src/main/java/com/jared/core/cache/redis/JedisClient.java
@@ -1,5 +1,7 @@
 package com.jared.core.cache.redis;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * Created with IntelliJ IDEA.
  * User: junde.yang
@@ -8,5 +10,53 @@ package com.jared.core.cache.redis;
  * To change this template use File | Settings | File Templates.
  */
 public class JedisClient {
+
+    private JedisManager jedisManager = JedisManager.getInstance();
+
+    public void set(String key, String value, int duration) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisManager.getResource();
+            if (jedis != null) {
+                jedis.setex(key, duration, value);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jedisManager.returnResource(jedis);
+        }
+    }
+
+    public String get(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisManager.getResource();
+            if (jedis != null) {
+                return jedis.get(key);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jedisManager.returnResource(jedis);
+        }
+        return null;
+    }
+
+
+    public void t(){
+        Jedis jedis = null;
+        try {
+            jedis = jedisManager.getResource();
+            if (jedis != null) {
+               // jedis.set
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jedisManager.returnResource(jedis);
+        }
+    }
+
+
 
 }
