09e4396f9064ed6459e13ea4df66f16372743998
yangjunde <363167160@qq.com>
2013-03-08 10:18:19 +0800

    config file

---
+++ core/src/main/java/com/jared/core/cache/redis/JedisClient.java
@@ -0,0 +1,12 @@
+package com.jared.core.cache.redis;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junde.yang
+ * Date: 13-3-7
+ * Time: 下午6:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class JedisClient {
+
+}
---
+++ core/src/main/java/com/jared/core/cache/redis/JedisManager.java
@@ -0,0 +1,58 @@
+package com.jared.core.cache.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junde.yang
+ * Date: 13-3-7
+ * Time: 上午10:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class JedisManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(JedisManager.class);
+
+    private static JedisManager instance = new JedisManager();
+
+    private JedisPool jedisPool;
+
+    public JedisManager() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxActive(300);
+        config.setMinIdle(2);
+        config.setMaxIdle(50);
+        config.setMaxWait(5000);
+        jedisPool = new JedisPool(config, "192.168.10.11", 6391);
+    }
+
+    public static JedisManager getInstance() {
+        return instance;
+    }
+
+    public Jedis getResource() {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+        } catch (Exception e) {
+            logger.error("Failed to get jedis resource, reason is " + e.getMessage());
+        }
+        return jedis;
+    }
+
+    public void returnResource(Jedis jedis){
+        if(jedis==null){
+            return;
+        }
+        try {
+            jedisPool.returnResource(jedis);
+        } catch (Exception e) {
+            logger.error("Failed to return jedis resource, reason is " + e.getMessage());
+        }
+    }
+
+}
