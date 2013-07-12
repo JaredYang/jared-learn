f425e72f710788d002936bec11d4d37938d7318c
yangjunde <363167160@qq.com>
2013-07-12 18:58:00 +0800

    ss

--- core/src/main/java/com/jared/core/lucene/LuceneTest.java
+++ core/src/main/java/com/jared/core/lucene/LuceneTest.java
@@ -4,11 +4,15 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.document.NumericField;
+import org.apache.lucene.index.*;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
@@ -31,63 +35,85 @@ public class LuceneTest {
     }
 
     private void writer() {
-        File indexDir = new File(INDEX_DIR);
-        File dataDir = new File(DATA_DIR);
-        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
-        File[] files = dataDir.listFiles();
-        IndexWriter indexWriter = null;
+        File file = new File(DATA_DIR);
+        Directory directory = null;
+        IndexWriter writer = null;
         try {
-            indexWriter = new IndexWriter(FSDirectory.open(indexDir), analyzer, IndexWriter.MaxFieldLength.LIMITED);
-
-            if (files != null && files.length > 0) {
-                for (File f : files) {
-                    Document document = new Document();
-                    document.add(new Field("path", f.getPath(), Field.Store.YES, Field.Index.ANALYZED));
-                    document.add(new Field("content", new FileReader(f)));
-                    indexWriter.addDocument(document);
-                }
-            }
-
-            indexWriter.optimize();
+            directory = FSDirectory.open(new File(INDEX_DIR));
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+            IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_31, analyzer);
+            indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+            writer = new IndexWriter(directory, indexWriterConfig);
+            indexDoc(writer, file);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (indexWriter != null) {
+            if (writer != null) {
                 try {
-                    indexWriter.close();
+                    writer.close();
                 } catch (IOException e) {
-
+                    e.printStackTrace();
                 }
             }
         }
+
     }
 
     private void query(String queryString) {
-        File indexDir = new File(INDEX_DIR);
         IndexReader indexReader = null;
-        Query query = null;
+        IndexSearcher indexSearch = null;
+        String field = "content";
         try {
-            indexReader = IndexReader.open(FSDirectory.open(indexDir), true);
-            Searcher searcher = new IndexSearcher(indexReader);
-            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
-            QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, "content", analyzer);
-            query = parser.parse(queryString);
-            TopScoreDocCollector topScoreDocCollector = TopScoreDocCollector.create(1000, false);
-            searcher.search(query, topScoreDocCollector);
+            indexReader = IndexReader.open(FSDirectory.open(new File(INDEX_DIR)));
+            indexSearch = new IndexSearcher(indexReader);
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+            QueryParser queryParser = new QueryParser(Version.LUCENE_31, field, analyzer);
+            Query query = queryParser.parse(queryString);
+            System.out.println("Searching for: " + query.toString(field));
+            TopDocs results = indexSearch.search(query, 100);
+            ScoreDoc[] hits = results.scoreDocs;
+            hits = indexSearch.search(query, 100).scoreDocs;
+            for (int i = 0; i < hits.length; i++) {
+                System.out.println("doc="+hits[i].doc+" score="+hits[i].score);
+                Document doc = indexSearch.doc(hits[i].doc);
+                String path = doc.get("path");
+                if (path != null) {
+                    System.out.println((i+1) + ". " + path);
+                    String title = doc.get("content");
+                    if (title != null) {
+                        System.out.println("   content: " + doc.get("content"));
+                    }
+                } else {
+                    System.out.println((i+1) + ". " + "No path for this document");
+                }
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
-        } finally {
-            indexReader.clone();
+        }finally {
+            if (indexSearch != null) {
+                try {
+                    indexSearch.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (indexReader != null) {
+                try {
+                    indexReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
     }
 
     public static void main(String[] args) {
         LuceneTest luceneTest = new LuceneTest();
-        //luceneTest.writer();
-        luceneTest.query("java");
+        luceneTest.writer();
+        luceneTest.query("Lucene");
     }
 
     private String getFileContent(String filePath) {
@@ -105,5 +131,52 @@ public class LuceneTest {
             e.printStackTrace();
         }
         return buffer.toString();
+    }
+
+    private void indexDoc(IndexWriter writer, File file) {
+        if (file.canRead()) {
+            if (file.isDirectory()) {
+                String[] files = file.list();
+                // an IO error could occur
+                if (files != null) {
+                    for (int i = 0; i < files.length; i++) {
+                        indexDoc(writer, new File(file, files[i]));
+                    }
+                }
+            } else {
+                FileInputStream fis;
+                try {
+                    fis = new FileInputStream(file);
+
+                } catch (FileNotFoundException e) {
+                    return;
+                }
+                try {
+                    Document document = new Document();
+                    Field pathField = new Field("path", file.getPath(), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
+                    pathField.setIndexOptions(FieldInfo.IndexOptions.DOCS_ONLY);
+                    document.add(pathField);
+                    NumericField modifiedField = new NumericField("modified");
+                    modifiedField.setLongValue(file.lastModified());
+                    document.add(modifiedField);
+                    document.add(new Field("content", new BufferedReader(new InputStreamReader(fis, "UTF-8"))));
+                    writer.addDocument(document);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (CorruptIndexException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
     }
 }
